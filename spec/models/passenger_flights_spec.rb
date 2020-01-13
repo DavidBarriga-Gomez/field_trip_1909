790ffedd797165d927a61ff08e3348ecff5a90b8
David Gomez <davdg25@gmail.com>
2020-01-13 09:16:16 -0700

    many to many for passenger and flights

---
+++ spec/models/passenger_flights_spec.rb
@@ -0,0 +1,29 @@
+require 'rail helper'
+
+RSpec.describe PassengerFlight, type: :model do
+
+end
+
+
+
+
+
+
+
+
+
+# \require 'rails_helper'
+#
+# describe ItemOrder, type: :model do
+#   describe "validations" do
+#     it { should validate_presence_of :order_id }
+#     it { should validate_presence_of :item_id }
+#     it { should validate_presence_of :price }
+#     it { should validate_presence_of :quantity }
+#     it { should validate_presence_of :status}
+#   end
+#
+#   describe "relationships" do
+#     it {should belong_to :item}
+#     it {should belong_to :order}
+#   end
